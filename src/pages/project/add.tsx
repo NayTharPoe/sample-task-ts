56883effda2c5b35528c46360434144367ebbab2
Saw Nay Thar Poe <100279951+sawnaytharpoe02@users.noreply.github.com>
2023-09-13 22:47:12 +0630

    added label

--- src/pages/project/add.tsx
+++ src/pages/project/add.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainLayout from '@/layouts/MainLayout';
 import { TextField } from '@mui/material';
 import { Stack } from '@mui/material';
@@ -7,7 +7,6 @@ import type { NextPageWithLayout } from '../_app';
 import { useForm, Controller } from 'react-hook-form';
 import { DatePicker } from '@mui/x-date-pickers';
 import { Button } from '@mui/material';
-import { error } from 'console';
 
 type FormValues = {
   projectName: string;
@@ -18,6 +17,7 @@ type FormValues = {
 };
 
 const AddNewProjectPage: NextPageWithLayout = () => {
+
   const form = useForm<FormValues>({
     defaultValues: {
       projectName: '',
@@ -30,6 +30,7 @@ const AddNewProjectPage: NextPageWithLayout = () => {
 
   const { register, handleSubmit, formState, control, setValue, setError, clearErrors } = form;
   const { errors } = formState;
+
   const onSubmit = (data: FormValues) => {
     if (!data.startDate) {
       setError('startDate', { type: 'manual', message: 'Start Date is required' });
@@ -51,58 +52,72 @@ const AddNewProjectPage: NextPageWithLayout = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)} noValidate>
       <Stack spacing={2}>
-        <TextField
-          label="Project Name"
-          {...register('projectName', { required: 'Project Name is required' })}
-          error={!!errors.projectName}
-          helperText={errors.projectName?.message}
-        />
-        <TextField
-          label="Language"
-          {...register('language', { required: 'Language is required' })}
-          error={!!errors.language}
-          helperText={errors.language?.message}
-        />
-        <TextField label="Description" {...register('description')}
-          error={!!errors.description}
-          helperText={errors.description?.message}
-        />
-        <Controller
-          name="startDate"
-          control={control}
-          render={({ field }) => (
-            <>
-              <DatePicker
-                label="Start Date"
-                value={field.value}
-                onChange={(newValue) =>
-                  field.onChange(newValue)
-                }
-              />
-              {errors.startDate && (
-                <span style={{ color: 'red' }}>{errors.startDate.message}</span>
-              )}
-            </>
-          )}
-        />
-        <Controller
-          name="endDate"
-          control={control}
-          render={({ field }) => (
-            <>
-              <DatePicker
-                label="End Date"
-                value={field.value}
-                onChange={(newValue) =>
-                  field.onChange(newValue)
-                }
-              />
-              {errors.endDate && (
-                <span style={{ color: 'red' }}>{errors.endDate.message}</span>
-              )}
-            </>
-          )}
-        />
+        <Stack spacing={1}>
+          <label htmlFor="projectName">Project Name</label>
+          <TextField
+            id='projectName'
+            {...register('projectName', { required: 'Project Name is required' })}
+            error={!!errors.projectName}
+            helperText={errors.projectName?.message}
+          />
+        </Stack>
+        <Stack spacing={1}>
+          <label htmlFor="language">Language</label>
+          <TextField
+            id='language'
+            {...register('language', { required: 'Language is required' })}
+            error={!!errors.language}
+            helperText={errors.language?.message}
+          />
+        </Stack>
+        <Stack spacing={1}>
+          <label htmlFor="description">Description</label>
+          <TextField id='description' {...register('description')}
+            error={!!errors.description}
+            helperText={errors.description?.message}
+          />
+        </Stack>
+
+        <Stack spacing={1}>
+          <label>Start Date Date</label>
+          <Controller
+            name="startDate"
+            control={control}
+            render={({ field }) => (
+              <>
+                <DatePicker
+                  value={field.value}
+                  onChange={(newValue) =>
+                    field.onChange(newValue)
+                  }
+                />
+                {errors.startDate && (
+                  <span style={{ color: 'red' }}>{errors.startDate.message}</span>
+                )}
+              </>
+            )}
+          />
+        </Stack>
+        <Stack spacing={1}>
+          <label>End Date</label>
+          <Controller
+            name="endDate"
+            control={control}
+            render={({ field }) => (
+              <>
+                <DatePicker
+                  value={field.value}
+                  onChange={(newValue) =>
+                    field.onChange(newValue)
+                  }
+                />
+                {errors.endDate && (
+                  <span style={{ color: 'red' }}>{errors.endDate.message}</span>
+                )}
+              </>
+            )}
+          />
+        </Stack>
         <Button type="submit">Create</Button>
         <Button>Cancel</Button>
       </Stack>
